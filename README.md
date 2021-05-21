36d1ac8a82ee6221d5f001bec53de0e557c9f08e
DefunctLizard <grantshandy@gmail.com>
2021-05-20 21:55:58 -0600

    add code : )

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # aplay
-Command Line Audio Player
+A Command Line Audio Player.
+
+Supports MP3, WAV, Vorbis, and flac audio.
+
+```
+$ cargo install aplay
+```
+
+## Command Line Arguments
+```
+aplay 0.1.0
+Grant Handy <grantshandy@gmail.com>
+A Command Line Audio Player
+
+USAGE:
+    aplay <FILE>
+
+FLAGS:
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+
+ARGS:
+    <FILE>    Sets the input file to use
+```
---
+++ src/main.rs
@@ -0,0 +1,64 @@
+use clap::{app_from_crate, crate_description, crate_authors, crate_name, crate_version, Arg};
+use colored::Colorize;
+use std::io::BufReader;
+use std::path::Path;
+use rodio::{Decoder, OutputStream, Sink};
+
+fn main() {
+    let matches = app_from_crate!()
+        .arg(Arg::with_name("FILE")
+            .help("Sets the input file to use")
+            .required(true)
+            .index(1))
+        .get_matches();
+
+    let (_stream, stream_handle) = match OutputStream::try_default() {
+        Ok((stream, stream_handle)) => (stream, stream_handle),
+        Err(error) => {
+            clap_error(error.to_string());
+            std::process::exit(1);
+        },
+    };
+
+    let sink = match Sink::try_new(&stream_handle) {
+        Ok(data) => data,
+        Err(error) => {
+            clap_error(error.to_string());
+            std::process::exit(1);
+        }
+    };
+
+    let path = Path::new(matches.value_of("FILE").unwrap());
+
+    if !path.is_file() {
+        clap_error("Couldn't find <FILE>");
+        std::process::exit(1);
+    };
+
+    let file = match std::fs::File::open(path) {
+        Ok(data) => data,
+        Err(error) => {
+            clap_error(error.to_string());
+            std::process::exit(1);
+        },
+    };
+
+    let file = BufReader::new(file);
+
+    let source = match Decoder::new(file) {
+        Ok(data) => data,
+        Err(error) => {
+            clap_error(error.to_string());
+            std::process::exit(1);
+        }
+    };
+    sink.append(source);
+
+    println!("Playing \"{}\"", path.file_name().unwrap().to_string_lossy());
+    sink.sleep_until_end();
+}
+
+fn clap_error<T: AsRef<str>>(text: T) {
+    eprintln!("{} {}\n", "error:".red().bold(), text.as_ref());
+    eprintln!("For more information try {}", "--help".green());
+}
